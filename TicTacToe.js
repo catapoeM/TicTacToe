72ac014cf6d2e9283f7708b1c712f91600526418
Poenaru Catalin <69017468+catapoeM@users.noreply.github.com>
2022-01-31 13:58:02 +0100

    Add files via upload

--- TicTacToe.js
+++ TicTacToe.js
@@ -17,6 +17,7 @@ function start() {
 	]
 	var coverAllButtons = 0;
 	var whoWins = 2;
+	var check_0 = 0, check_x = 0;
 	tableCreate();
 	// Here we create the table and disable the start button
 	function tableCreate() {
@@ -38,73 +39,65 @@ function start() {
 		    }
 	    }
 	}
+
 	function pickCell(idCell) {
 		++coverAllButtons;
 		cell[idCell].setAttribute("disabled", "");
-		var check = 0;
-		// In the first if condition we mark the clicked cell with "X" and check if X player
-		// wins or not
 		if (ticTac == 1) {
 			cell[idCell].innerHTML = "<span style='font-size:100px'>X</span>";
 			combinations_x.push(idCell);
-			// In this loops we check every line from winning combinations with the clicked 
-			// cells
-			for (var i = 0; combinations_x.length > 2 && i < winning_combinations.length; ++i) {
-				for (var j = 0; j < 3; ++j) {
-					for (var k = 0; k < combinations_x.length; ++k) {
-						if (winning_combinations[i][j] == combinations_x[k]) {
-							++check;
-						}
-					}
-				}
-				// If "X" wins reset game
-				if (check == 3) {
-					whoWins = 1;
-					resetGame();
-				}
-				check = 0;
-			}
+			info.innerHTML = "<span style='font-size:40px'>0's turn</span>";
 			ticTac = 0;
-		// In the second condition we mark the clicked cell with "0" and check if 0 player
-		// wins or not
 		}else if (ticTac == 0) {
 			cell[idCell].innerHTML = "<span style='font-size:100px'>0</span>";
 			combinations_0.push(idCell);
-			// In this loops we check every line from winning combinations with the clicked 
-			// cells
-			for (var i = 0; combinations_0.length > 2 && i < winning_combinations.length; ++i) {
-				for (var j = 0; j < 3; ++j) {
-					for (var k = 0; k < combinations_0.length; ++k) {
-						if (winning_combinations[i][j] == combinations_0[k]) {
-							++check;
-						}
-					}
-				}
-				// If "0" wins reset game
-				if (check == 3) {
-					whoWins = 0;
-					resetGame();			
-				}
-				check = 0;
-			}
+			info.innerHTML = "<span style='font-size:40px'>X's turn</span>";
 			ticTac = 1;
 		}
-		// We show display the information about player's turn and the winner, or TIE
-		// if no one win
-		if (ticTac == 0) {
-			info.innerHTML = "<span style='font-size:40px'>0's turn</span>";
-		}else {
-			info.innerHTML = "<span style='font-size:40px'>X's turn</span>";
+		// In the first for loop we check every line of combination array
+		for (var i = 0; combinations_x.length > 2 || combinations_0.length > 2 && i < winning_combinations.length; ++i) {
+			// This second for checks the specific line of winning combination array
+			for (var j = 0; j < 3; ++j) {
+				// this third for verify the equality between elements of the specific line and the x or 0 combinations
+				for (var k = 0; k < combinations_x.length || k < combinations_0.length; ++k) {
+					if (winning_combinations[i][j] == combinations_x[k]) {
+						++check_x;
+					}else if (winning_combinations[i][j] == combinations_0[k]) {
+						++check_0;
+					}
+				}
+			}
+			if (check_x == 3) {
+				whoWins = 1;
+				winner();
+				
+			}else if (check_0 == 3) {
+				whoWins = 0;
+				winner();
+			}else if (coverAllButtons == 9 && whoWins == 2) {
+				coverAllButtons = 0;
+				check_x = 0;
+				tie();
+			}
+			check_x = 0;
+			check_0 = 0;
 		}
-	    if (whoWins == 0) {
+	}		
+	
+	function winner() {
+		if (whoWins == 0) {
 	    	info.innerHTML = "<span style='font-size:40px'>'0' wins!</span>";
 	    }else if (whoWins == 1) {
 	    	info.innerHTML = "<span style='font-size:40px'>'X' wins!</span>";
-	    }else if (coverAllButtons == 9) {
-	    	info.innerHTML = "<span style='font-size:40px'>No one wins, it is TIE!</span>";
-	    	resetGame();
 	    }
-    }
+	    return resetGame();
+	}
+
+	function tie() {
+		info.innerHTML = "<span style='font-size:40px'>No one wins, it is TIE!</span>";
+		return resetGame();
+	}
+		  
     function resetGame() {
     	for (var id = 1; id < 10; ++id) {
 			cell[id].setAttribute("disabled", "");
